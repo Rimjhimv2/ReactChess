4bafc61c17f3915fb048af6701fd2aa10fc63651
Rimjhimv2 <rimjhimv30@gmail.com>
2025-11-20 00:51:06 +0530

    Initial commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import bgImage from '../../assets/images/bgImage.jpg';
+import PieceArray from '../PieceArray';
+import axios from "axios"
+import { login } from '../../store/authSlice';
+function Login() {
+    const dispatch = useDispatch();
+    
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [error, setError] = useState('');
+    React.useEffect(() => {
+        axios.get("http://localhost:3000/profile", {
+            withCredentials: true
+        })
+            .then(res => {
+                const data = res.data;
+                dispatch(login(data));
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error fetching profile:', error);
+            });
+    }, [dispatch]);
+   
+    const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch('http://localhost:3000/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            }),
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            dispatch(login(data));  // Dispatch login action with user data
+            navigate('/profile');
+        } else {
+            setError(data.error || 'Login failed');
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        setError('Internal server error');
+    }
+};
+
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div
+            className="w-screen min-h-screen bg-cover bg-no-repeat bg-center flex items-center justify-center"
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
+            <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl border border-gray-200 p-8 lg:p-12 lg:p-16 rounded-xl shadow-lg w-11/12 max-w-md lg:max-w-lg xl:max-w-xl">
+                <PieceArray />
+                <h2 className="lg:text-4xl lg:text-5xl sm:text-3xl font-semibold text-center text-white mb-12">Login</h2>
+                <form className="space-y-6" onSubmit={handleLogin}>
+                    <div>
+                        <label htmlFor="email" className="block text-lg my-2 font-medium text-white">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="tracking-wider placeholder-opacity-75 text-xl placeholder-gray-100 mt-1 p-2 w-full rounded-md bg-gray-200 bg-opacity-30 text-white border border-gray-400 focus:ring focus:ring-blue-500"
+                            placeholder="you@example.com"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-lg my-2 font-medium text-white">Password</label>
+                        <div className="relative">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="tracking-wider text-xl placeholder-opacity-75 placeholder-gray-100 mt-1 p-2 w-full rounded-md bg-gray-200 bg-opacity-30 text-white border border-gray-400 focus:ring focus:ring-blue-500"
+                                placeholder="••••••••"
+                                required
+                            />
+                            <span className="absolute right-3 top-3" onClick={togglePasswordVisibility}>
+                                {!showPassword ? <i className="fas fa-eye-slash text-white"></i> : <i className="fas fa-eye text-white"></i>}
+                            </span>
+                        </div>
+                    </div>
+                    {error && <p className="text-red-500">{error}</p>}
+                    <div className="text-center">
+                        <button
+                            type="submit"
+                            className="text-xl px-4 w-full py-2 my-3 bg-transparent text-gray-100 rounded-md border border-purple-300 hover:bg-purple-600 hover:border-purple-600 hover:text-white transition duration-300 ease-in-out"
+                        >
+                            Login
+                        </button>
+                    </div>
+                    <div className="text-center text-lg text-white">
+                        Don't have an account? <Link to="/signup" className="text-purple-300 hover:text-white">Sign Up</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/components/Home.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Typing from 'react-typing-effect';
+import bgImage from '../assets/images/bgImage.jpg';
+function Home() {
+  const authStatus = useSelector(state => state.auth.status);
+  const userData = useSelector(state => state.auth.userData);
+
+  return (
+    <div
+      className="w-screen min-h-screen flex items-center justify-center"
+      style={{ 
+        backgroundImage: `url(${bgImage})`, 
+        backgroundSize: 'cover', 
+        backgroundRepeat: 'no-repeat', 
+        backgroundPosition: 'center' 
+      }}
+    >
+      <div className="font-serif tracking-wide bg-gray-800 flex flex-col bg-opacity-30 backdrop-filter backdrop-blur-xl border border-gray-500 p-8 rounded-xl shadow-lg text-center w-11/12 lg:w-2/3 lg:w-1/2 xl:w-1/2 h-2/3 lg:h-1/2">
+        <h1 className="text-3xl lg:text-5xl tracking-normal font-bold font-mono text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 mb-4 ">
+          <Typing 
+            text={['Welcome to Chess Master']} 
+            speed={100} 
+            eraseSpeed={50} 
+            typingDelay={200} 
+            eraseDelay={5000}
+          />
+        </h1>
+
+        <div className='description text-center align-middle h-2/3 text-md md:text-xl lg:text-2xl text-gray-100 mb-4'>
+        
+            Experience the ultimate chess journey with Chess Master. Challenge your friends in local multiplayer, or take on global opponents with our advanced socket integration. Sharpen your skills with intricate puzzles, or test your strategies against Stockfish, the world’s strongest chess engine. For those moments of frustration, switch to "Always Win" mode and enjoy a flawless victory every time
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-4">
+          {authStatus === "true" && userData.username? (
+            <>'
+              <Link to="/modeselector" className="block bg-green-400 bg-opacity-40 text-white py-3 px-6 rounded-lg text-lg lg:text-2xl w-full  hover:bg-opacity-50 transition duration-300 border border-white hover:bg-gray300 hover:text-yellow-400 transform transition duration-300 hover:scale-105">
+                Continue 
+              </Link>
+              
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="block bg-gray-400 bg-opacity-40 text-white py-3 px-6 rounded-lg text-lg lg:text-2xl w-full lg:w-1/2 hover:bg-opacity-50 transition duration-300 border border-white hover:bg-gray-300 hover:text-blue-200 transform transition duration-300 hover:scale-105">
+                Login
+              </Link>
+              <Link to="/signup" className="block bg-gray-400 bg-opacity-40 text-white py-3 px-6 rounded-lg text-lg lg:text-2xl w-full lg:w-1/2 hover:bg-opacity-50 transition duration-300 border border-white hover:bg-gray-300 hover:text-blue-200 transform transition duration-300 hover:scale-105">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
